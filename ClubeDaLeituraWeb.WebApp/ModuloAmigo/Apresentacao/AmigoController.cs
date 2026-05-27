cc0e4d6022e8f4aeb38f2cbf4aa25de0c7711b11
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-05-27 18:15:18 -0300

    Adiciona views Listar e Cadastrar do ModuloAmigo

--- ClubeDaLeituraWeb.WebApp/ModuloAmigo/Apresentacao/AmigoController.cs
+++ ClubeDaLeituraWeb.WebApp/ModuloAmigo/Apresentacao/AmigoController.cs
@@ -1,8 +1,65 @@
+using ClubeDaLeituraWeb.WebApp.ModuloAmigo.Dominio;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClubeDaLeituraWeb.WebApp.ModuloAmigo.Apresentacao;
 
 public class AmigoController : Controller
 {
+    private readonly IRepositorioAmigo repositorioAmigo;
 
+    public AmigoController(IRepositorioAmigo repositorioAmigo)
+    {
+        this.repositorioAmigo = repositorioAmigo;
+    }
+
+    [HttpGet]
+    public ActionResult Listar()
+    {
+        List<Amigo> amigos = repositorioAmigo.SelecionarTodos();
+
+        List<ListarAmigosViewModel> listarVms = new List<ListarAmigosViewModel>();
+
+        foreach (Amigo a in amigos)
+        {
+            ListarAmigosViewModel viewModel = new ListarAmigosViewModel(
+                a.Id,
+                a.Nome,
+                a.NomeResponsavel,
+                a.Telefone
+            );
+
+            listarVms.Add(viewModel);
+        }
+
+        return View(listarVms);
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        CadastrarAmigoViewModel cadastrarVm = new CadastrarAmigoViewModel(
+            string.Empty,
+            string.Empty,
+            string.Empty
+        );
+
+        return View(cadastrarVm);
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarAmigoViewModel cadastrarVm)
+    {
+        if (!ModelState.IsValid)
+            return View(cadastrarVm);
+
+        Amigo novoAmigo = new Amigo(
+            cadastrarVm.Nome,
+            cadastrarVm.NomeResponsavel,
+            cadastrarVm.Telefone
+        );
+
+        repositorioAmigo.Cadastrar(novoAmigo);
+
+        return RedirectToAction(nameof(Listar));
+    }
 }
--- ClubeDaLeituraWeb.WebApp/ModuloAmigo/Apresentacao/AmigoViewModels.cs
+++ ClubeDaLeituraWeb.WebApp/ModuloAmigo/Apresentacao/AmigoViewModels.cs
@@ -1,8 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ClubeDaLeituraWeb.WebApp.ModuloAmigo.Apresentacao;
 
-public record ListarAmigosViewModel();
+public record ListarAmigosViewModel(
+    string Id,
+    string Nome,
+    string NomeResponsavel,
+    string Telefone
+);
 
-public record CadastrarAmigoViewModel();
+public record CadastrarAmigoViewModel(
+    [Required(ErrorMessage = "O campo \"Nome\" deve ser preenchido.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo \"Nome\" deve conter entre 3 e 100 caracteres.")]
+    string Nome,
+
+    [Required(ErrorMessage = "O campo \"Nome do responsável\" deve ser preenchido.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo \"Nome do responsável\" deve conter entre 3 e 100 caracteres.")]
+    string NomeResponsavel,
+
+    [Required(ErrorMessage = "O campo \"Telefone\" deve ser preenchido.")]
+    [RegularExpression(@"^\d{10,11}$", ErrorMessage = "O campo \"Telefone\" deve conter entre 10 e 11 dígitos.")]
+    string Telefone
+);
 
 public record EditarAmigoViewModel();
 
